cba22efd1aad66a0fc107b4faa98b723929c9355
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-10-06 19:04:11 +0200

    update: queries chapter 1 - Select and From

--- queries01.sql
+++ queries01.sql
@@ -1,4 +1,4 @@
-
+/* Select and From */
 -- Select everything
 SELECT	*
 FROM	[dbo].[Melbourne Housing Master]
@@ -14,3 +14,22 @@ SELECT	[Suburb] AS [Förort]
 		,[Type] AS [Bostadstyp]
 FROM	[dbo].[Melbourne Housing Master]
 
+-- Math Operations - Create DisplayTable
+SELECT	[Suburb]
+		,[Rooms]
+		,([Final Price] / 1000) AS [Price_In_Thousands]
+		,(([Final Price] + 5000)/1000) AS [Modified_Price]
+FROM	[dbo].[Melbourne Housing Master]
+
+-- String Operations - Create DisplayTable from two other tables
+SELECT	[Suburb]
+		,[Address]
+		,[Suburb] + ' - ' + [Address] AS [Suburb - Address]
+		,CONCAT([Suburb],' - ',[Address]) AS [Suburb - Address 2] 
+FROM	[dbo].[Melbourne Housing Master]
+
+-- Selecting a certaint count of rows (pagination)
+SELECT TOP(100) Suburb, Address
+FROM [dbo].[Melbourne Housing Master]
+
+/* Filtering Data - Where */
