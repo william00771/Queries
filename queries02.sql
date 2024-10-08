c372e9fb1c94f6f3db880ba99559c9fef555d143
William Lindberg <117780994+william00771@users.noreply.github.com>
2024-10-08 17:29:05 +0200

    update: queries intermediate chapter - Stored Procedures. Finished!

--- queries02.sql
+++ queries02.sql
@@ -5,6 +5,9 @@
 	3. Replacing Characters
 
 	You would typically put the new "cleansed" data in a new table ex  [Restaurant - Master (Cleansed)]
+
+	Checking for duplicate values:
+	OBS -> It's Incredibly dangerous for data analysis! 
 */
 
 /*NULL VALUES - MAKE SURE THEY ARE NOT NULL*/
@@ -129,12 +132,339 @@ SELECT * FROM [Restaurant - Master (Cleansed)]
 
 
 
+
 ------------------------------------------------------------------------------------------------------------------------
 /*Window Functions - Increases functionality of a dataset*/
 
+-- (7) WINDOW FUNCTION - INTRODUCE REVENUE COLUMN
+    SELECT [Restaurant Name]
+          ,[Meal Release Date]
+          ,[Meal]
+          ,[Meal Description]
+          ,[Meal Sides]
+          ,[Cost]
+          ,[Quantity]
+		  ,([Quantity]*[Cost]) AS [Revenue]
+		  ,[Meal Comment]
+  INTO [dbo].[Restaurant - Master (Cleansed) - Revenue]
+  FROM [dbo].[Restaurant - Master (Cleansed)]
 
+  SELECT * FROM [Restaurant - Master (Cleansed)]
+
+-- (8) WINDOW FUNCTION - ROW COUNT -> Each row ranked by the restaurant name 1-29 in this case b first since b is first in alpabet, can replace with cost or whatever you want
+  SELECT ROW_NUMBER() 
+			OVER (ORDER BY [Restaurant Name]) 
+				 AS [Row Number]
+	    ,[Restaurant Name]
+        ,[Meal Release Date]
+        ,[Meal]
+        ,[Meal Description]
+        ,[Meal Sides]
+        ,[Cost]
+        ,[Quantity]
+		,[Revenue]
+		,[Meal Comment]
+  FROM [dbo].[Restaurant - Master (Cleansed) - Revenue]
+
+-- Here we just replace with Cost and make it asc order
+  SELECT ROW_NUMBER() 
+			OVER (ORDER BY [Cost] ASC) 
+				 AS [Row Number]
+	    ,[Restaurant Name]
+        ,[Meal Release Date]
+        ,[Meal]
+        ,[Meal Description]
+        ,[Meal Sides]
+        ,[Cost]
+        ,[Quantity]
+		,[Revenue]
+		,[Meal Comment]
+  FROM [dbo].[Restaurant - Master (Cleansed) - Revenue]
+  
+-- (9) WINDOW FUNCTION - RANK -> Each row ranked by the descending Revenue so rank 1 made the most revenue in this case, we can add WHERE here aswell to filter rank based on certain values
+  SELECT [Restaurant Name]
+        ,[Meal Release Date]
+        ,[Meal]
+        ,[Meal Description]
+        ,[Meal Sides]
+        ,[Cost]
+        ,[Quantity]
+		,[Revenue]
+		,RANK() 
+			OVER (ORDER BY [Revenue] DESC) 
+				 AS [Rank]
+		,[Meal Comment]
+  FROM [dbo].[Restaurant - Master (Cleansed) - Revenue]
+
+-- (10) WINDOW FUNCTION - RANK (Introducing PARTITION) - Seperate ranking system for specific field and subfield. So same [Restaurant Name] value will all be grouped into one ranking system that goes from 1-whatever based on ex [Revenue]. So you seperate them.
+  SELECT [Restaurant Name]
+        ,[Meal Release Date]
+        ,[Meal]
+        ,[Meal Description]
+        ,[Meal Sides]
+        ,[Cost]
+        ,[Quantity]
+		,[Revenue]
+        ,RANK() 
+			OVER (PARTITION BY [Restaurant Name] 
+				  ORDER BY [Revenue] DESC)
+				  AS [Meal Rank]
+		,[Meal Comment]
+  FROM [dbo].[Restaurant - Master (Cleansed) - Revenue]
 
 
 
 ------------------------------------------------------------------------------------------------------------------------
-/*Data Cleansing*/
+/*Running Total*/
+-- See how a certain metric is progressing. 'cumulative frequency'
+-- (11) RUNNING TOTAL - Summing everytime there is a new object. Basically just summarizing all values progressivly as they are added. You can obviously have other operations here aswell.
+  SELECT [Restaurant Name]
+        ,[Meal Release Date]
+        ,[Meal]
+        ,[Meal Description]
+        ,[Meal Sides]
+        ,[Cost]
+        ,[Quantity]
+		,[Revenue]
+		,SUM([Revenue]) 
+			OVER (ORDER BY [Revenue]) 
+				 AS [Overall Running Total]
+		,[Meal Comment]
+  FROM [dbo].[Restaurant - Master (Cleansed) - Revenue]
+
+
+-- (12) RUNNING TOTAL (PARTITION)
+  SELECT [Restaurant Name]
+        ,[Meal Release Date]
+        ,[Meal]
+        ,[Meal Description]
+        ,[Meal Sides]
+        ,[Cost]
+        ,[Quantity]
+		,[Revenue]
+		,SUM([Revenue]) 
+			OVER (PARTITION BY [Restaurant Name] 
+			ORDER BY [Revenue]) 
+			AS [Restaunt Running Total]
+		,[Meal Comment]
+  FROM [dbo].[Restaurant - Master (Cleansed) - Revenue]
+
+
+
+------------------------------------------------------------------------------------------------------------------------
+/*Conversion (Preperation step for next section)*/
+SELECT [Restaurant]
+      ,CAST([Customer %] AS INT) AS [Customer %]
+into [UDEMY SQL Course - Intermediate].[dbo].[Restaurant - Customer Reach]
+FROM [UDEMY SQL Course - Intermediate].[dbo].[Restaurant - Customer Reach_]
+
+
+
+------------------------------------------------------------------------------------------------------------------------
+/*Subqueries AKA Nested Queries*/
+-- One query from the result of another query
+USE [UDEMY SQL Course]
+GO
+
+-- (13) SUBQUERY (INTO)
+SELECT A.* 
+INTO [Pay - Appended]
+FROM 
+	(
+	  SELECT [Name]
+			,[Pay]
+	  FROM [dbo].[Female Pay]
+  
+	  UNION ALL
+  
+	  SELECT [Name]
+			,[Pay]
+	  FROM [dbo].[Male Pay] 
+	) AS A
+
+
+-- (14) SUBQUERY (Filtering) - Tables that only have a customer reach % over 20% -> Includes a filter based on the entries from another table
+-- Customer Reach is the only table that has the Customer % Reach parameter. The [Restaurant - Master (Cleansed) - Revenue] has all the other data
+	SELECT A.[Restaurant Name]
+          ,A.[Meal Release Date]
+          ,A.[Meal]
+          ,A.[Meal Description]
+          ,A.[Meal Sides]
+          ,A.[Cost]
+          ,A.[Quantity]
+          ,A.[Revenue]
+          ,A.[Meal Comment]
+    FROM [Restaurant - Master (Cleansed) - Revenue] as A
+	WHERE [Restaurant Name] IN (SELECT B.[Restaurant]
+							   FROM [Restaurant - Customer Reach] as B
+							   WHERE [Customer %] > 20) 
+
+------------------------------------------------------------------------------------------------------------------------
+/*View - Virtual Table, that doesn't actually take up additional storage in the database*/
+-- Gets created inside Views Folder - SQL Treats the view like a normal table. Fantastic to experiment on for example since it's a 1/1 mirror of the original table but not the actual original table
+-- It does allow flexibility. Cause it does not actually use any storage. But it does draw from the orignal table dataset.
+
+-- (15) VIEWS
+    CREATE VIEW [Missing Meal Sides]
+	AS 
+	SELECT [Restaurant Name]
+          ,[Meal Release Date]
+          ,[Meal]
+          ,[Meal Description]
+          ,[Meal Sides]
+          ,[Cost]
+          ,[Quantity]
+          ,[Revenue]
+          ,[Meal Comment]
+	FROM [Restaurant - Master (Cleansed) - Revenue]
+	WHERE [Meal Sides] = '--N/A--'
+ 
+-- (16) SELECTING THE VIEW
+  SELECT *
+  FROM [Missing Meal Sides]
+
+-- ALTER the VIEW
+    ALTER VIEW [Missing Meal Sides]
+	AS 
+	SELECT [Restaurant Name]
+          ,[Meal Release Date]
+          ,[Meal]
+          ,[Meal Description]
+          ,[Meal Sides]
+          ,[Cost]
+          ,[Quantity]
+          ,[Revenue]
+          ,[Meal Comment]
+	FROM [Restaurant - Master (Cleansed) - Revenue]
+	WHERE [Meal Sides] = '--N/A--'
+ 
+
+
+------------------------------------------------------------------------------------------------------------------------
+/*Checking for duplicate values & Cleaning.*/
+-- OBS -> It's Incredibly dangerous for data analysis! 
+
+-- (17) CHECKING FOR DUPLICATES
+  SELECT [Restaurant]
+        ,[Ranking]
+        ,[Description]
+	    ,COUNT(*) AS [Row Count]
+  FROM [Restaurant Rankings (Duplicates)]
+  GROUP BY [Restaurant]
+          ,[Ranking]
+          ,[Description]
+
+-- We can see what the duplicates are and by how many
+  SELECT [Restaurant]
+        ,[Ranking]
+        ,[Description]
+	    ,COUNT(*) AS [Row Count]
+  FROM [Restaurant Rankings (Duplicates)]
+  GROUP BY [Restaurant]
+          ,[Ranking]
+          ,[Description]
+  HAVING COUNT(*) > 1
+
+-- We can remove duplicates by aggregating the table against itself
+  SELECT [Restaurant]
+        ,[Ranking]
+        ,[Description]
+  FROM [Restaurant Rankings (Duplicates)]
+  GROUP BY [Restaurant]
+          ,[Ranking]
+          ,[Description]
+
+
+
+------------------------------------------------------------------------------------------------------------------------
+/*Using Variables in SQL*/
+/*
+	DECLARE @name datatype()
+	SET @name = valueAssignedToVariable
+*/
+-- (18) VARIABLE (Filter)
+  DECLARE @restaurant nvarchar(100)
+  SET @restaurant = 'Data Pizza'
+
+  	SELECT [Restaurant Name]
+          ,[Meal Release Date]
+          ,[Meal]
+          ,[Meal Description]
+          ,[Meal Sides]
+          ,[Cost]
+          ,[Quantity]
+          ,[Revenue]
+          ,[Meal Comment]
+	FROM [Restaurant - Master (Cleansed) - Revenue]
+	WHERE [Restaurant Name] = @Restaurant
+
+-- (19) VARIABLE (Table) - Cool we can recreate the query using variables
+  DECLARE @table nvarchar(100) 
+  DECLARE @filter nvarchar(100)
+  DECLARE @SQL_query nvarchar(max)
+
+  SET @table = '[Restaurant - Master (Cleansed) - Revenue]'
+  SET @filter = 'Data Pizza'
+  SET @SQL_query = '
+
+  	SELECT [Restaurant Name]
+          ,[Meal Release Date]
+          ,[Meal]
+          ,[Meal Description]
+          ,[Meal Sides]
+          ,[Cost]
+          ,[Quantity]
+          ,[Revenue]
+          ,[Meal Comment]
+	FROM ' + @table + ' 
+	WHERE [Restaurant Name] = ''' + @filter + ''' 
+	ORDER BY [Revenue] 
+	
+	'
+
+  PRINT @SQL_query 
+  EXEC (@SQL_query)
+
+
+
+------------------------------------------------------------------------------------------------------------------------
+/*Stored Procedures - Way of saving queries INSIDE your database. Fantastic!*/
+-- Variables can be used in here aswell! 
+-- They get stored in the Programmability -> Stored Procedures folder
+-- You can then run these stored procedures whenever you want!
+-- EXEC Runs the stored procedure
+
+--(20) STORED PROCEDURE (STORING CODE) - Just a simple duplicate query that we want to create as a stored procedure
+  CREATE PROCEDURE [Duplicate Code]
+  AS
+
+  SELECT [Restaurant]
+        ,[Ranking]
+        ,[Description]
+	    ,COUNT(*) AS [Row Count]
+  FROM [Restaurant Rankings (Duplicates)]
+  GROUP BY [Restaurant]
+          ,[Ranking]
+          ,[Description]
+  HAVING COUNT(*) > 1
+
+-- (21) STORED PROCEDURE (Variable) - 
+  CREATE PROCEDURE [Restaurant Select]
+	@Restaurant nvarchar(100)
+  AS
+    
+	SELECT [Restaurant Name]
+          ,[Meal Release Date]
+          ,[Meal]
+          ,[Meal Description]
+          ,[Meal Sides]
+          ,[Cost]
+          ,[Quantity]
+          ,[Revenue]
+          ,[Meal Comment]
+	FROM [Restaurant - Master (Cleansed) - Revenue]
+	WHERE [Restaurant Name] = @Restaurant
+
+-- (22) RUNNING STORED PROCEDURE
+    EXEC  [dbo].[Restaurant Select]
+		  @Restaurant = 'Big Data Burger'
